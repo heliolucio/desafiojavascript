d0ef2adf63c16ca7dca3fb928be91a386c49e29c
Logon Aluno <logonaluno@local.com>
2020-02-13 21:03:26 -0300

    Atualização do código

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -36,4 +36,12 @@ class NegociacaoController {
         this._inputValor.value = 0.0;
         this._inputData.focus();   
     }
+
+    apaga() {
+        this._listaNegociacoes.esvazia();
+        this._negociacoesView.update(this._listaNegociacoes);
+        this._mensagem.texto = 'Negociações apagadas com sucesso';
+        this._mensagemView.update(this._mensagem);
+       }
+       
 }
